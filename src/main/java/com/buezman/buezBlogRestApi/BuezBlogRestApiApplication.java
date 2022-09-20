f874f10ed3250b85a626836737cb56a33f7bb322
Buezman <54446003+Buezman@users.noreply.github.com>
2022-09-20 16:13:37 +0100

    first commit

---
+++ src/main/java/com/buezman/buezBlogRestApi/BuezBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.buezman.buezBlogRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuezBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuezBlogRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buezman/buezBlogRestApi/BuezBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buezman.buezBlogRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuezBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
